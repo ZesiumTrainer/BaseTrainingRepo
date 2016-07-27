bd021ebd931758b4018235c20461da81271e6f68
rajko.bacic <bacic.rajko@gmail.com>
2016-07-27 15:31:36 +0200

    Initial State

--- Osnove/Osnove/App_Start/RouteConfig.cs
+++ Osnove/Osnove/App_Start/RouteConfig.cs
@@ -12,12 +12,6 @@ namespace Osnove.App_Start
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapRoute(
-               name: "Default",
-               url: "{controller}/{action}",
-               defaults: new { controller = "Init", action = "Default" }
-           );
         }
     }
 }
